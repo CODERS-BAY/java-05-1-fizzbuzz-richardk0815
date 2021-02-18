bc51a2df7e8fd9f31f1ec2d1023e72a3a5287399
Richard Kolm <richard.kolm@gmx.at>
2021-02-18 11:45:26 +0100

    fizzBuzz

---
+++ src/fizzBuzz.java
@@ -0,0 +1,16 @@
+public class fizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i +"fizzbuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
